8db303e8f4fde89fde10e0769475c0543a87645b
Xiaomin <lixiaomin9579@hotmail.com>
2020-06-11 22:18:28 -0500

    add python gpu profiling code

--- gpu/gpuUtilization.py
+++ gpu/gpuUtilization.py
@@ -1,6 +1,27 @@
 import py3nvml.nvidia_smi as smi
-smi.nvmlInit()
-handle = smi.nvmlDeviceGetHandleByIndex(0)
+import datetime  
+import time
 
-res = smi.nvmlDeviceGetUtilizationRates(handle)
-print('gpu: {}%, gpu-mem: {}%'.format(res.gpu, res.memory))
+smi.nvmlInit()
+
+handle0 = smi.nvmlDeviceGetHandleByIndex(0)
+handle1 = smi.nvmlDeviceGetHandleByIndex(1)
+handle2 = smi.nvmlDeviceGetHandleByIndex(2)
+handle3 = smi.nvmlDeviceGetHandleByIndex(3)
+
+current_time = datetime.datetime.now()
+print(current_time + 'start gpu measurement')
+time_count = 0
+while True:
+    print('Time:{}s'.format(time_count))
+    res0 = smi.nvmlDeviceGetUtilizationRates(handle0)
+    res1 = smi.nvmlDeviceGetUtilizationRates(handle1)
+    res2 = smi.nvmlDeviceGetUtilizationRates(handle2)
+    res3 = smi.nvmlDeviceGetUtilizationRates(handle3)
+
+    print('gpu: {}%, gpu-mem: {}%'.format(res0.gpu, res0.memory))
+    print('gpu: {}%, gpu-mem: {}%'.format(res1.gpu, res1.memory))
+    print('gpu: {}%, gpu-mem: {}%'.format(res2.gpu, res2.memory))
+    print('gpu: {}%, gpu-mem: {}%'.format(res3.gpu, res3.memory))
+    time_count += 1
+    sleep(1)
